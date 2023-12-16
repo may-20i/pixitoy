6902d60470041ccdca80059d7e78c59adf3aad2b
_ <_>
2023-12-16 06:35:47 -0600

    Fixes typing.

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,9 +1,19 @@
-import { animate, generateSpriteMap, initialize } from "./utils"
+import { animate, generateCells, generateSpriteMap, initialize } from "./utils"
 import { CONSTANTS } from "./constants"
 
 const { app, container, graphics } = initialize();
 
+const map = generateCells(10);
 const spriteMap = generateSpriteMap<keyof typeof CONSTANTS.spriteImportDefinitions>(CONSTANTS.spriteImportDefinitions);
+const tile_size = 16;
+const keys = Object.keys(spriteMap);
+
+for (const key of keys) {
+  const sprite = spriteMap[key];
+
+  sprite.width = tile_size;
+  sprite.height = tile_size;
+}
 
 const draw = () => {
   app.renderer.clear(); // Clear the renderer
